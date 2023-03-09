ab7da4f937a0ab4864f6092c563c0c892d2b7178
Anders Boyum <Acboyum@gmail.com>
2023-03-09 11:15:43 -0600

    finished core questions

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -23,8 +23,12 @@ console.log(supplyChanges[1]);
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
+console.log(supplyChanges.pop());
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges.push(25);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -33,15 +37,45 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 
+for (let i = 0; i <= supplyChanges.length -1; i++) {
+    if (supplyChanges[i] > 0){
+        console.log(`Added ${supplyChanges[i]} parts.`);
+    }
+    else if (supplyChanges[i] == 0) {
+        console.log('No change');
+    }
+    else {
+        console.log(`Removed ${supplyChanges[i]} parts.`);
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for (parts of supplyChanges) {
+    if (parts > 0){
+        console.log(`Added ${parts} parts.`);
+    }
+    else if (parts == 0) {
+        console.log('No change.');
+    }
+    else {
+        console.log(`Removed ${parts} parts.`);
+    }
+}
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+
+let totalParts;
+
+for (let i = 0; i <= supplyChanges - 1; i++) {
+    totalParts += supplyChanges[i];
+}
+console.log(totalParts);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
